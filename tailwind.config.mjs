54be58f4e7892710a5b1df6eaa1682a71f95118c
Kevin Eiserman <eisermanka@gmail.com>
2025-01-16 14:22:19 -0500

    Add astro icon

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Space Grotesk Variable", ...defaultTheme.fontFamily.sans],
+        mono: ["Space Mono", ...defaultTheme.fontFamily.mono],
+      },
+    },
   },
   plugins: [],
 };
